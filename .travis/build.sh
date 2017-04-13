976d455377ed2ca006ec5f848058fb5266b4c35c
Miro Kropacek <miro.kropacek@gmail.com>
2017-04-13 19:50:14 +1000

    Travis CI deployment

---
+++ .travis/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+TMP="$1"
+OUT="$2"
+
+sed -e "s/\"\(\.5\)\"/\"\1-${SHORT_ID}\"/;" src/version.h > src/version.h.tmp && mv src/version.h.tmp src/version.h
+VERSION="$(grep QED_VERSION src/version.h | sed -e 's/^.*\"v\(.*\)\".*/\1/;')"."$(grep QED_REVISION src/version.h | sed -e 's/^.*\"\.\(.*\)\".*/\1/;')"
+make CROSS=yes
+
+# TODO: generate HYP
+mkdir -p "${TMP}/qed"
+cp "src/qed_new.app" "${TMP}/qed/qed.app"
+cp "en/qed_en.rsc" "${TMP}/qed/qed.rsc"
+cp "en/readme.txt" "${TMP}/qed"
+cp "en/readme.old" "${TMP}/qed"
+cp -r "en/syntax" "${TMP}/qed"
+cp "dist/qed.cfg" "${TMP}/qed"
+cp "dist/icons.rsc" "${TMP}/qed"
+cp -r "dist/kurzel" "${TMP}/qed"
+cp -r "dist/treiber" "${TMP}/qed"
+
+# TODO: generate HYP
+mkdir -p "${TMP}/qed/de"
+cp "src/qed.rsc" "${TMP}/qed/de"
+cp -r "tools/syntax" "${TMP}/qed/de"
+cp "dist/liesmich.txt" "${TMP}/qed/de"
+cp "dist/liesmich.old" "${TMP}/qed/de"
+
+find "${TMP}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
+mkdir -p "${OUT}" && cd "${TMP}" && zip -r -9 "${OUT}/${PROJECT}-${VERSION}.zip" *
