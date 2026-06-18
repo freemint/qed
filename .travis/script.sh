bafccf3b59ba356b47f085ed3e3ade9c6310864a
Miro Kropacek <miro.kropacek@gmail.com>
2026-06-18 14:16:34 +1000

    Redirect uploads to joska's server
    Also remove .travis leftovers

--- .travis/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-# -e: Exit immediately if a command exits with a non-zero status.
-# -u: Treat unset variables as an error when substituting.
-# -x: Display expanded script commands
-
-make cpu=${CPU_TARGET} installdir=${INSTALL_DIR} install
